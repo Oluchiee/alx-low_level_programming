4fcc0bb54cb6c9aab3004503e778907f6198db22
Oluchiee <debezeh@gmail.com>
2023-02-16 20:53:35 -0800

    Task 5

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,5 +10,5 @@ int main(void)
 	printf("Size of a long int: %lu bytes(s)\n", sizeof(long int));
 	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long int));
 	printf("Size of a float: %lu bytes(s)\n", sizeof(float));
-return (0);
+	return (0);
 }
