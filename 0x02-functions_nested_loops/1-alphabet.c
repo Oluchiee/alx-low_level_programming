dd806de4d6b25d5532307b083d88b5c05b871368
Oluchiee <debezeh@gmail.com>
2023-02-21 14:35:40 -0800

    Task 0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char c = 'a'
+
+	while (c <= 'z');
+	{
+		_putchar (c);
+		c++;
+	}
+	putchar ('\n');
+	return (0);
+}
