eb565ee7e63889ff8ded0cac02a273b465c1ebb2
Oluchiee <debezeh@gmail.com>
2023-02-21 16:32:38 -0800

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,1 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - function that prints 10X the alphabet 
+ * followed by new line.
+ * Return: Always 0.
+ */
 
+
+
+void print_alphabet_x10(void)
+{
+	int a;
+	char b;
+
+	for (a = 1 ; a <= 10 ; a++)
+	{
+		for (b = 'a' ; b <= 'z' ; b++)
+			_putchar(b);
+		_putchar('\n');
+	}
+}
