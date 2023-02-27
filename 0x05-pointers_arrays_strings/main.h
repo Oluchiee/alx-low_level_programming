f2a3a98dd347ea027cdcdb31222bd817f50f67dc
Oluchiee <debezeh@gmail.com>
2023-02-27 08:29:48 -0800

    Task 3

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,7 +6,7 @@
  * Description: contains all functions prototype used in this project
  */
 
-
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
