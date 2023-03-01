077241a8bcdd01a516acd846d437f82c86d62b41
Oluchiee <debezeh@gmail.com>
2023-03-01 10:42:38 -0800

    Task 10

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
-
 /*
    * write your line of code here...
    * Remember:
    * - you are not allowed to use a
-   * - you are not allowed to modify p                        * - only one statement                                     * - you are not allowed to code anything else than this line of code
+   * - you are not allowed to modify p
+   *   only one statement
+   *   you are not allowed to code anything else than this line of code
    */
 int main(void)
 {
