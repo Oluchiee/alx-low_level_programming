a86e47644f70038e0693d4ddcf53ce22af428016
Oluchiee <debezeh@gmail.com>
2023-04-10 21:23:12 -0700

    second

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,11 +9,11 @@
  */
 void print_binary(unsigned long int n)
 {
-	if (e >> 0)
+	if (n >> 0)
 	{
-		if (e >> 1)
-			print_binary(e >> 1);
-		_putchar((e & 1) + '0');
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
 	}
 	else
 	{
